ba4b0d4bbe867263dc3677a2f0a5579567fa6958
Drew Taylor <drew@taylorspot.com>
2026-02-04 21:22:38 -0500

    Run the audit, fix caused issues

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import SignedInLanding from '@/features/mixology/experiences/SignedInLanding';
 import SignedOutLanding from '@/features/mixology/experiences/SignedOutLanding';
 import { getCurrentUser } from '@/features/mixology/server/auth';
 
+export const dynamic = 'force-dynamic';
+
 export default async function HomePage() {
   const user = await getCurrentUser();
 
